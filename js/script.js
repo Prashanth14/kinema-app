760ab535f79099dddb7f8ae22d50ea1071fad322
Prashanth14 <kadireprashanthreddy1@gmail.com>
2023-11-28 19:21:20 -0500

    fetch data from API and display popular T-shows

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ const global = {
     currentPage: window.location.pathname,
 };
 
+//Display popular Movies
 async function displayPopularMovies(){
     const {results} = await fetchAPIData('movies/popular');
 
     results.forEach(movie => {
-        console.log(movie);
         const div = document.createElement('div');
         div.classList.add('card');
 
@@ -38,6 +38,44 @@ async function displayPopularMovies(){
     });
 }
 
+//Display popular Tv-Shows
+async function displayPopularShows(){
+    const {results} = await fetchAPIData('tv/popular');
+    console.log(results);
+    results.forEach(show => {
+        // console.log(show);
+        const div = document.createElement('div');
+        div.classList.add('card');
+
+        div.innerHTML = `
+          <a href="tv-details.html?id=${show.id}">
+          ${
+            show.poster_path ?
+            `<img
+                src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+                class="card-img-top"
+                alt="${show.original_name}"
+            />`
+            :
+            `<img
+                src="images/no-image.jpeg"
+                class="card-img-top"
+                alt="${show.original_name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.original_name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date
+              }</small>
+            </p>
+          </div>`;
+
+          document.querySelector('#popular-shows').appendChild(div);
+    });
+}
+
 //fetch data from the TMDB API
 async function fetchAPIData(endpoint){
     const options = {
@@ -47,9 +85,11 @@ async function fetchAPIData(endpoint){
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDVkNzk2OTVmM2MyYzAyNmQyODI0MGZkYjYxOTA2NCIsInN1YiI6IjY1NjYxZjBjMTdiNWVmMDEwNTQ4ZDM3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Rs40domB8G7RrQIv60j00NE15iS9eVE_-zcoTLZ6dVA'
         }
       };
-    const response = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options);
+    showSpinner();
+    const response = await fetch(`https://api.themoviedb.org/3/${endpoint}?language=en-US&page=1`, options);
 
     const data = await response.json();
+    hideSpinner();
     return data;
 }
 
@@ -63,6 +103,14 @@ function highlightActiveLink(){
     });
 }
 
+function showSpinner(){
+    document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner(){
+    document.querySelector('.spinner').classList.remove('show');
+}
+
 //Init App
 function init(){
     switch(global.currentPage){
@@ -71,7 +119,7 @@ function init(){
             displayPopularMovies();
             break;
         case '/shows.html':
-            console.log('Shows');
+            displayPopularShows();
             break;
         case '/movie-details.html':
             console.log('Movie Details');
