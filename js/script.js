653bfc02b1fc788f95b03b0fb5ef32f9cbdbfd44
Prashanth14 <kadireprashanthreddy1@gmail.com>
2023-12-01 13:51:30 -0500

    added pagination for search

--- js/script.js
+++ js/script.js
@@ -242,7 +242,12 @@ async function search(){
     global.search.term = urlParams.get('search-term');
 
     if(global.search.term !== '' && global.search.type !== null){
-        const {results, total_pages, page} = await searchAPIData();
+        const {results, total_pages, page, total_results} = await searchAPIData();
+
+        global.search.page = page;
+        global.search.totalPages = total_pages;
+        global.search.totalResults = total_results;
+
         if(results.length === 0){
             showAlert('No results found');
             return;
@@ -257,8 +262,14 @@ async function search(){
 }
 
 //displaySerachResults
-
 function displaySerachResults(results){
+
+    //clear previous results
+    document.querySelector('#search-results').innerHTML = '';
+    document.querySelector('#search-results-heading').innerHTML = '';
+    document.querySelector('#pagination').innerHTML = '';
+
+
     results.forEach(result => {
         const div = document.createElement('div');
         div.classList.add('card');
@@ -287,8 +298,50 @@ function displaySerachResults(results){
             </p>
           </div>`;
 
+          document.querySelector('#search-results-heading').innerHTML = `
+          <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>`
+
           document.querySelector('#search-results').appendChild(div);
     });
+    displayPagination();
+}
+
+//Create and Display Pagination for search
+function displayPagination(){
+    const div = document.createElement('div');
+    div.classList.add('pagination');
+    div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+    `;
+
+    document.querySelector('#pagination').appendChild(div);
+
+    //Disable previous button if on first page
+    if(global.search.page === 1){
+        document.querySelector('#prev').disabled = true;
+    }
+
+    //Disable next button if on last page
+    if(global.search.page === global.search.totalPages){
+        document.querySelector('#next').disabled = true;
+    }
+
+    //Next Page
+    document.querySelector('#next').addEventListener('click', async() =>{
+        global.search.page++;
+        const { results, total_pages} = await searchAPIData();
+        displaySerachResults(results);
+    })
+
+     //Previous Page
+     document.querySelector('#prev').addEventListener('click', async() =>{
+        global.search.page--;
+        const { results, total_pages} = await searchAPIData();
+        displaySerachResults(results);
+    })
+
 }
 
 // Display Slider Movies
@@ -365,12 +418,13 @@ async function searchAPIData(){
         }
       };
     showSpinner();
-    const response = await fetch(`https://api.themoviedb.org/3/search/${global.search.type}?language=en-US&query=${global.search.term}`, options);
+    const response = await fetch(`https://api.themoviedb.org/3/search/${global.search.type}?language=en-US&query=${global.search.term}&page=${global.search.page}`, options);
 
     const data = await response.json();
     hideSpinner();
     return data;
 }
+
 
 //Highlight the active link
 function highlightActiveLink(){
